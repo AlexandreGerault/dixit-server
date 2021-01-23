0dfb8ef8ed296f744db498a9f58e2842459e2036
Alexandre Gérault <alexandre@hexium.io>
2021-01-23 18:54:35 +0100

    fix(functions): add return type

--- src/functions/RandomString.ts
+++ src/functions/RandomString.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
-const randomString = (size: number) => {
+const randomString = (size: number): string => {
   return crypto.randomBytes(size).toString('base64').substring(0, size);
 };
 
